2c806ab08707ad8816195dd30c97d8f43ea9a377
liawchunvoon <liawcv@tarc.edu.my>
2025-02-16 23:25:26 +0800

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World 123!'),
+          child: Text('Hello World 123456!'),
         ),
       ),
     );
